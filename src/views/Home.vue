23db90e8e799cde52834f290d666f353691be202
Curtis <curtis@drumeo.com>
2020-05-12 16:07:42 -0700

    Testing ground.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,25 +2,61 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
 
-    <LuButton
-      to="about"
-      color="green"
-      type="outline"
-      size="small"
-      disabled
+    <LuRow
+      tag="main"
+      vAlign="center"
+      hAlign="left"
+      :wrap="true"
     >
-      Test Text
-    </LuButton>
+      <LuColumn
+        tag="section"
+        :columnWidths="{
+          xs: '100%',
+          md: '50%',
+          lg: '30%',
+        }"
+      >
+        <LuButton
+          to="about"
+          color="green"
+          type="outline"
+          disabled
+        >
+          Test Text
+        </LuButton>
+      </LuColumn>
+      <LuColumn
+        tag="section"
+        :columnWidths="{
+          xs: '30%',
+          md: '50%',
+          lg: '100%',
+        }"
+      >
+        <LuButton
+          to="about"
+          color="green"
+          type="outline"
+          disabled
+        >
+          Test Text
+        </LuButton>
+      </LuColumn>
+    </LuRow>
   </div>
 </template>
 
 <script>
 import LuButton from '../components/button/LuButton.vue';
+import LuRow from '../components/grid/LuRow.vue';
+import LuColumn from '../components/grid/LuColumn.vue';
 
 export default {
   name: 'Home',
   components: {
     LuButton,
+    LuRow,
+    LuColumn,
   },
 };
 </script>
