de483f73ea1b88ee1602f35e869690a30d330d81
Curtis <curtis@drumeo.com>
2020-05-13 13:13:54 -0700

    Add a quick text component

---
+++ src/components/text/LuText.vue
@@ -0,0 +1,17 @@
+<template>
+  <component :is="tag">
+    <slot></slot>
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'LuText',
+  props: {
+    tag: {
+      type: String,
+      default: () => 'p',
+    },
+  },
+};
+</script>
