b0eca5ae7ac6560c1772520121e6d5c5547b86e0
Curtis <curtis@drumeo.com>
2020-05-13 14:03:24 -0700

    Add LuText component

--- src/components/text/LuText.vue
+++ src/components/text/LuText.vue
@@ -1,16 +1,43 @@
 <template>
-  <component :is="tag">
+  <component :is="tag" :class="defaultClassList">
     <slot></slot>
   </component>
 </template>
 
 <script>
+import './LuText.scss';
+import colors from '../../assets/js/mixins/colors';
+import spacing from '../../assets/js/mixins/spacing';
+
 export default {
   name: 'LuText',
+  mixins: [colors, spacing],
   props: {
     tag: {
       type: String,
       default: () => 'p',
+    },
+    transform: {
+      type: String,
+      default: () => undefined,
+    },
+  },
+  computed: {
+    defaultClassList() {
+      return [
+        'lu-text',
+        this.textColor,
+        this.textTransformClass,
+        this.marginClass,
+        this.paddingClass,
+      ];
+    },
+    textTransformClass() {
+      const transforms = {
+        uppercase: 'text-uppercase',
+        capitalize: 'text-capitalize',
+      };
+      return transforms[this.transform] || '';
     },
   },
 };
