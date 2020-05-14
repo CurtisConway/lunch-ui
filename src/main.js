24b45e2e32bff35fb1d8d079d01c36f4fca8d970
Curtis <curtis@drumeo.com>
2020-05-14 14:56:05 -0700

    Fix the entry file for testing

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import LetusUI from '../dist/letus-ui.umd.min';
+import LetusUI from '../index';
 import './assets/sass/app.scss';
 
 Vue.config.productionTip = false;
