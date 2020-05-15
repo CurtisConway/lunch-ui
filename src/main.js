92d2fea95a2f1a8a9525814cfc970812ff23daa0
Curtis <curtis.conway14@gmail.com>
2020-05-15 12:20:16 -0700

    Remove router directory

--- src/main.js
+++ src/main.js
@@ -1,10 +1,36 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router';
+import VueRouter from 'vue-router';
 import LunchUI from '../index';
 import './assets/sass/app.scss';
 
+import Home from './views/Home.vue';
+
 Vue.config.productionTip = false;
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+});
 
 Vue.use(LunchUI);
 
